deb69ee70d753f49f01a95078a7473956b358e10
Tkxiong89 <txiong401@gmail.com>
2022-01-26 19:57:19 -0600

    solved compress string problem

--- algorithmic_strategies_exercise/05_compress_str.rb
+++ algorithmic_strategies_exercise/05_compress_str.rb
@@ -1,7 +1,25 @@
 # Write a method, compress_str(str), that accepts a string as an arg.
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
-
+def compress_str(string)
+    i=0
+    compress_str = ""
+    while i < string.length
+        char = string[i]
+        count = 0
+        while char == string[i]
+            count += 1
+            i += 1
+        end
+        if count > 1
+            compress_str += ( count.to_s + char )
+        else
+            compress_str += char
+        end
+        
+    end
+    return compress_str
+end
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
